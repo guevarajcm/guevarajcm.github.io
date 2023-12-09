04e6879c8b428a956779a311fb865506b66adb5a
Juan Guevara <guevarajcm@gmail.com>
2023-12-09 09:49:23 -0600

    Machines are now added with a JSON format

--- assets/js/searches.js
+++ assets/js/searches.js
@@ -1,4 +1,31 @@
+// Carga los artículos desde el archivo JSON y los muestra en la página
+function loadArticles() {
+    fetch('assets/articles.json')
+        .then(response => response.json())
+        .then(articles => {
+            const container = document.getElementById('articles-container');
+            articles.forEach(article => {
+                const articleHTML = `
+                    <article class="col-6 col-12-xsmall work-item">
+                        <a href="${article.url}" class="image fit thumb"><img src="${article.image}" alt="${article.title}" /></a>
+                        <h3>${article.title}</h3>
+                        <div class="tags">
+                            ${article.tags.map(tag => `<span>${tag}</span>`).join(' ')}
+                        </div>
+                    </article>
+                `;
+                container.innerHTML += articleHTML;
+            });
+        })
+        .catch(error => console.error('Error al cargar los artículos:', error));
+}
+
+// Maneja la funcionalidad de búsqueda
 document.addEventListener('DOMContentLoaded', function() {
+    // Cargar los artículos al cargar la página
+    loadArticles();
+
+    // Agregar evento de entrada para la funcionalidad de búsqueda
     document.getElementById('search-input').addEventListener('input', function(e) {
         var searchTerm = e.target.value.toLowerCase();
 
@@ -18,3 +45,4 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
